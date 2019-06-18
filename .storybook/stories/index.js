8d0c1d7e390e99ffbd8ad4c9b8f632731221eaee
zana-a <zana200.a@gmail.com>
2019-06-18 15:44:23 +0100

    add story for button

--- .storybook/stories/index.js
+++ .storybook/stories/index.js
@@ -1,17 +1,8 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { Button } from "@storybook/react/demo";
+
+import Button from "./components/Button";
 
 import "../../dist/lemonade.css";
 
-storiesOf("Button", module)
-  .add("with text", () => <Button>Hello Button</Button>)
-  .add("with emoji", () => (
-    <Button>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
-
-storiesOf("Hello", module).add("Dumbo", () => <span>You dumbo!</span>);
+storiesOf("Button", module).add("Base", () => <Button />);
