7d03d79284ae1db7fc2242cde8dac079a50921fd
zana-a <zana200.a@gmail.com>
2019-07-11 13:33:09 +0100

    add grunt ! source map not working

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,9 @@
 module.exports = function(grunt) {
-  // Project configuration.
   grunt.initConfig({
+    watch: {
+      files: "./src/**/*.less",
+      tasks: ["less"]
+    },
     less: {
       development: {
         options: {
@@ -12,8 +15,22 @@ module.exports = function(grunt) {
       },
       production: {
         options: {
-          paths: ["./dist/"],
-          compress: true
+          paths: ["./dist"],
+          plugins: [
+            new (require("less-plugin-autoprefix"))({
+              browsers: ["last 2 versions"]
+            }),
+            new (require("less-plugin-clean-css"))({
+              options: {
+                advanced: true
+              }
+            })
+          ],
+          syncImport: true,
+          banner: "/*! Lemonade 0.0.1 **/",
+          stripBanners: true,
+          sourceMap: false,
+          sourceMapFilename: "./dist/lemonade.min.map"
         },
         files: {
           "./dist/lemonade.min.css": "./src/lemonade.less"
@@ -22,7 +39,7 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks("assemble-less");
-
-  grunt.registerTask("default", ["less"]);
+  grunt.registerTask("default", ["watch"]);
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-contrib-watch");
 };
