a6f34754d5cbdba2e7b4d1a96f734ee94b110565
Zana <zana.alan.200@gmail.com>
2019-07-11 18:56:57 +0000

    seperated tasks for gulp ! less loops not working

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 let gulp = require("gulp");
 let less = require("gulp-less");
-let LessAutoprefixer = require("less-plugin-autoprefix");
+let lessAutoprefixer = require("less-plugin-autoprefix");
+let lessLists = require("less-plugin-lists");
 let sourcemaps = require('gulp-sourcemaps');
 let cleanCSS = require('gulp-clean-css');
 
-const autoprefixer = new LessAutoprefixer({
+const autoprefixer = new lessAutoprefixer({
   browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
 });
 
@@ -21,15 +22,26 @@ const metaData = {
   "src": "./src/lemonade.less",
   "dist": "./dist"
 }
+//
+// gulp.task("less:dev", async () => {
+//   return gulp.src(metaData.src)
+//     .pipe(sourcemaps.init())
+//     .pipe(less({ plugins: [autoprefixer] }))
+//     .pipe(sourcemaps.write("./"))
+//     .pipe(cleanCSS(cleanCSSOptionsDev))
+//     .pipe(gulp.dest(metaData.dist));
+// });
 
-gulp.task("default", async () => {
+gulp.task("less:prod", async () => {
   return gulp.src(metaData.src)
     .pipe(sourcemaps.init())
-    .pipe(less({ plugins: [autoprefixer] }))
+    .pipe(less({ plugins: [autoprefixer, lessLists] }))
     .pipe(sourcemaps.write("./"))
-    .pipe(cleanCSS(cleanCSSOptionsDev, (details) => {
+    .pipe(cleanCSS(cleanCSSOptionsProd, (details) => {
       console.log(`\nOriginal Size: ${details.name}: ${details.stats.originalSize}`);
       console.log(`Minified Size: ${details.name}: ${details.stats.minifiedSize}\n`);
     }))
     .pipe(gulp.dest(metaData.dist));
 });
+
+gulp.task("default", gulp.parallel("less:prod"));
