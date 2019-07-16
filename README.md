18cade4181f157d8a220741d4a9c7131f6afe69a
Zana <zana@Zanas-MacBook-Pro.local>
2019-07-16 01:37:39 +0100

    add header file to lemonade.css

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
 # Lemonade
 
-Lemonade is a general purpose framework written in the `.less` preprocessor to be as stylish and extensible as possible.
+Lemonade is a general purpose framework written in the Less preprocessor to be as stylish and extensible as possible.
 
 Please come back soon for more information. If you are still curious, don't be afraid to poke around or message me at <zana200@icloud.com>.
--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,61 @@
+let fs = require("fs");
 let gulp = require("gulp");
 let less = require("gulp-less");
 let lessAutoprefixer = require("less-plugin-autoprefix");
 let lessLists = require("less-plugin-lists");
-let sourcemaps = require('gulp-sourcemaps');
-let cleanCSS = require('gulp-clean-css');
+let sourcemaps = require("gulp-sourcemaps");
+let cleanCSS = require("gulp-clean-css");
+let gap = require("gulp-append-prepend");
+
+const pacakgeData = JSON.parse(fs.readFileSync("./package.json"));
 
 const autoprefixer = new lessAutoprefixer({
-  browsers: ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
+  browsers: [
+    "last 2 version",
+    "safari 5",
+    "ie 8",
+    "ie 9",
+    "opera 12.1",
+    "ios 6",
+    "android 4"
+  ]
 });
 
 const cleanCSSOptionsDev = {
   level: 2,
   format: "beautify"
-}
+};
 
 const cleanCSSOptionsProd = {
   level: 2
-}
+};
 
 const metaData = {
-  "src": "./src/lemonade.less",
-  "dist": "./dist"
-}
-//
-// gulp.task("less:dev", async () => {
-//   return gulp.src(metaData.src)
-//     .pipe(sourcemaps.init())
-//     .pipe(less({ plugins: [autoprefixer] }))
-//     .pipe(sourcemaps.write("./"))
-//     .pipe(cleanCSS(cleanCSSOptionsDev))
-//     .pipe(gulp.dest(metaData.dist));
-// });
+  locations: {
+    src: "./src/lemonade.less",
+    dist: "./dist"
+  },
+  fileHeader: `/*!\n * ${pacakgeData.name} (${pacakgeData.version}) ~ ${pacakgeData.license}\n * ${pacakgeData.repository.url}\n */`
+};
 
 gulp.task("less:prod", async () => {
-  return gulp.src(metaData.src)
-    .pipe(sourcemaps.init())
+  return gulp
+    .src(metaData.locations.src)
+    .pipe(gap.prependText(metaData.fileHeader))
     .pipe(less({ plugins: [autoprefixer, lessLists] }))
+    .pipe(sourcemaps.init())
+    .pipe(
+      cleanCSS(cleanCSSOptionsProd, details => {
+        console.log(
+          `\nOriginal Size: ${details.name}: ${details.stats.originalSize}`
+        );
+        console.log(
+          `Minified Size: ${details.name}: ${details.stats.minifiedSize}\n`
+        );
+      })
+    )
     .pipe(sourcemaps.write("./"))
-    .pipe(cleanCSS(cleanCSSOptionsProd, (details) => {
-      console.log(`\nOriginal Size: ${details.name}: ${details.stats.originalSize}`);
-      console.log(`Minified Size: ${details.name}: ${details.stats.minifiedSize}\n`);
-    }))
-    .pipe(gulp.dest(metaData.dist));
+    .pipe(gulp.dest(metaData.locations.dist));
 });
 
 gulp.task("default", gulp.parallel("less:prod"));
