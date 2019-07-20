33711ce395935d00e558a701ce467055f5b22ddd
Zana <zana@Zanas-MacBook-Pro.local>
2019-07-20 13:26:41 +0100

    bug fixes and storybook customisation

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,6 +1,69 @@
-import { configure } from "@storybook/react";
+import { configure, addParameters } from "@storybook/react";
+import yourTheme from "./lemonadeTheme";
 
 const req = require.context("../src", true, /.stories.js$/);
+
+addParameters({
+  options: {
+    /**
+     * show story component as full screen
+     * @type {Boolean}
+     */
+    isFullscreen: false,
+    /**
+     * display panel that shows a list of stories
+     * @type {Boolean}
+     */
+    showNav: true,
+    /**
+     * display panel that shows addon configurations
+     * @type {Boolean}
+     */
+    showPanel: false,
+    /**
+     * where to show the addon panel
+     * @type {('bottom'|'right')}
+     */
+    panelPosition: "right",
+    /**
+     * regex for finding the hierarchy separator
+     * @example:
+     *   null - turn off hierarchy
+     *   /\// - split by `/`
+     *   /\./ - split by `.`
+     *   /\/|\./ - split by `/` or `.`
+     * @type {Regex}
+     */
+    hierarchySeparator: /\/|\./,
+    /**
+     * regex for finding the hierarchy root separator
+     * @example:
+     *   null - turn off multiple hierarchy roots
+     *   /\|/ - split by `|`
+     * @type {Regex}
+     */
+    hierarchyRootSeparator: /\|/,
+    /**
+     * sidebar tree animations
+     * @type {Boolean}
+     */
+    sidebarAnimations: true,
+    /**
+     * enable/disable shortcuts
+     * @type {Boolean}
+     */
+    enableShortcuts: true,
+    /**
+     * show/hide tool bar
+     * @type {Boolean}
+     */
+    isToolshown: true,
+    /**
+     * theme storybook
+     */
+    theme: yourTheme
+  }
+});
 
 function loadStories() {
   require("./stories/index.js");
