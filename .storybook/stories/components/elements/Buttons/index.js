619f29d2149fd1bb8040f281fe0409f71535b136
zana-a <zana200.a@gmail.com>
2019-08-14 22:26:13 +0100

    add button outline

--- .storybook/stories/components/elements/Buttons/index.js
+++ .storybook/stories/components/elements/Buttons/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 const BtnColors = [
-  "light",
-  "dark",
   "gray",
   "red",
   "pink",
@@ -18,6 +16,8 @@ const BtnColors = [
   "orange"
 ];
 
+const BtnSizes = ["sm", "md", "lg", "xl"];
+
 const ButtonBase = props => {
   return (
     <div className='p-2'>
@@ -32,10 +32,25 @@ const Button = BtnColors.map(color => {
   return <ButtonBase class={"btn btn-" + color} name={color} />;
 });
 
+const ButtonOutline = BtnColors.map(color => {
+  return <ButtonBase class={"btn btn-outline btn-" + color} name={color} />;
+});
+
+const ButtonSize = BtnSizes.map(size => {
+  return (
+    <ButtonBase
+      class={"btn btn-blue btn-" + size}
+      name={"Button " + size.toUpperCase()}
+    />
+  );
+});
+
 function Buttons() {
   return (
     <div>
       <div>{Button}</div>
+      <div>{ButtonSize}</div>
+      <div>{ButtonOutline}</div>
     </div>
   );
 }
