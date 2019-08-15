cfab45dbcd50aff32e659e2038084810512244c6
zana-a <zana200.a@gmail.com>
2019-08-15 17:59:16 +0100

    add input color types

--- .storybook/stories/components/elements/Inputs/index.js
+++ .storybook/stories/components/elements/Inputs/index.js
@@ -52,7 +52,53 @@ function Input() {
       </div>
       <div className='t-white p-4 bg-black'>
         <h2>Outline</h2>
-        <InputExample type='input-outline-light' />
+        <InputExample type='input-outline' />
+      </div>
+      <div className='bg-white'>
+        <div className='p-4'>
+          <input
+            type='text'
+            className='input input-success'
+            placeholder='All Good!'
+          />
+        </div>
+        <div className='p-4'>
+          <input
+            type='text'
+            className='input input-danger'
+            placeholder='Unsuccessful!'
+          />
+        </div>
+        <div className='p-4'>
+          <input
+            type='text'
+            className='input input-warning'
+            placeholder='Warning!'
+          />
+        </div>
+      </div>
+      <div className='bg-gray-9'>
+        <div className='p-4'>
+          <input
+            type='text'
+            className='input input-success input-outline'
+            placeholder='All Good!'
+          />
+        </div>
+        <div className='p-4'>
+          <input
+            type='text'
+            className='input input-danger input-outline'
+            placeholder='Unsuccessful!'
+          />
+        </div>
+        <div className='p-4'>
+          <input
+            type='text'
+            className='input input-warning input-outline'
+            placeholder='Warning!'
+          />
+        </div>
       </div>
     </div>
   );
