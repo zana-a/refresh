f87b6b9159dbeee483367f8ea8e4fdde5c3aeabd
zana-a <zana200.a@gmail.com>
2019-08-15 01:23:47 +0100

    add input-outline-light

--- .storybook/stories/components/elements/Inputs/index.js
+++ .storybook/stories/components/elements/Inputs/index.js
@@ -10,11 +10,50 @@ const InputText = props => {
   );
 };
 
+const InputExample = porps => {
+  return (
+    <div>
+      <input
+        placeholder='text'
+        name='firstname'
+        className={"input input-gray " + porps.type}
+        autoComplete='off'
+      />
+      <br />
+      <br />
+      <input
+        placeholder='text'
+        name='lastname'
+        className={"input input-gray " + porps.type}
+        autoComplete='off'
+      />
+      <br />
+      <br />
+      <input
+        type='submit'
+        value='Submit'
+        className='btn btn-blue'
+        autoComplete='off'
+      />
+    </div>
+  );
+};
+
 function Input() {
   return (
     <div className='p-4'>
-      <InputText class='input-red' placeholder='Hello' />
-      <button className='btn btn-gray btn-outline'>Button</button>
+      <div>
+        <InputText class='input-red' placeholder='Hello' />
+        <button className='btn btn-blue'>Go!</button>
+      </div>
+      <div className='py-4'>
+        <h2>Normal</h2>
+        <InputExample />
+      </div>
+      <div className='t-white p-4 bg-black'>
+        <h2>Normal</h2>
+        <InputExample type='input-outline-light' />
+      </div>
     </div>
   );
 }
