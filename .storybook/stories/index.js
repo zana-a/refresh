c5560215f4caf4a6c308d89ce99e34384b4920e7
zana-a <zana200.a@gmail.com>
2019-09-16 22:47:50 +0100

    add Content

--- .storybook/stories/index.js
+++ .storybook/stories/index.js
@@ -18,3 +18,33 @@ Storybook.storiesOf("Standard Package|Reset", module)
 Storybook.storiesOf("Standard Package|Base", module)
   .add("Introduction", () => <BaseIntroduction />)
   .add("Heading", () => <Heading />);
+
+Storybook.storiesOf("Lab|Default", module).add("Test Lab", () => {
+  return (
+    <div>
+      <div className='Content'>
+        <h1 style={{ width: "60%", marginLeft: "auto", marginRight: "auto" }}>
+          Royal rumble as the Queen decides to declare prince William as King!
+        </h1>
+        <img src='https://placeimg.com/900/300/people' />
+      </div>
+
+      <div className='Content' style={{ width: "60%", margin: "0 auto" }}>
+        <h3>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          efficitur sed nunc eu porttitor. Fusce facilisis tortor eu fermentum
+          aliquet. In vel vehicula odio.
+        </h3>
+        <h2>Charles arrested?</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+          efficitur sed nunc eu porttitor. Fusce facilisis tortor eu fermentum
+          aliquet. In vel vehicula odio, id lacinia nisi. Cras pharetra, felis
+          lobortis cursus ultrices, massa nisi congue risus, nec fringilla justo
+          sapien at felis.
+        </p>
+        <img src='https://placeimg.com/500/300/people' />
+      </div>
+    </div>
+  );
+});
