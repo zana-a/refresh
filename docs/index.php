78ccc1be3373ed9fd5bf620657cc203dacc03faa
zana-a <zana200.a@gmail.com>
2019-10-23 00:43:55 +0100

    add basic border atom

--- docs/index.php
+++ docs/index.php
@@ -1,63 +1,27 @@
 <?php
-function rgb(int $a, int $b, int $c) {
-    $hex = sprintf("#%02x%02x%02x", $a, $b, $c);
-    return $hex;
-}
 
-$_DEFAULT_BLUE =  rgb(0, 122, 255);
-$_DEFAULT_GREEN =  rgb(52, 199, 89);
-$_DEFAULT_INDIGO =  rgb(88, 86, 214);
-$_DEFAULT_ORANGE =  rgb(255, 149, 0);
-$_DEFAULT_PINK =  rgb(255, 45, 85);
-$_DEFAULT_PURPLE =  rgb(175, 82, 222);
-$_DEFAULT_RED =  rgb(255, 59, 48);
-$_DEFAULT_TEAL =  rgb(90, 200, 250);
-$_DEFAULT_YELLOW =  rgb(255, 204, 0);
-
-$_DARK_BLUE =  rgb(10, 132, 255);
-$_DARK_GREEN =  rgb(48, 209, 88);
-$_DARK_INDIGO =  rgb(94, 92, 230);
-$_DARK_ORANGE =  rgb(255, 159, 10);
-$_DARK_PINK =  rgb(255, 55, 95);
-$_DARK_PURPLE =  rgb(191, 90, 242);
-$_DARK_RED =  rgb(255, 69, 58);
-$_DARK_TEAL =  rgb(100, 210, 255);
-$_DARK_YELLOW =  rgb(255, 214, 10);
-
-$_DEFAULT_GRAY_1 =  rgb(142, 142, 147);
-$_DEFAULT_GRAY_2 =  rgb(174, 174, 178);
-$_DEFAULT_GRAY_3 =  rgb(199, 199, 204);
-$_DEFAULT_GRAY_4 =  rgb(209, 209, 204);
-$_DEFAULT_GRAY_5 =  rgb(229, 229, 234);
-$_DEFAULT_GRAY_6 =  rgb(242, 242, 247);
-
-$_DARK_GRAY_1 =  $_DEFAULT_GRAY_1;
-$_DARK_GRAY_2 =  rgb(99, 99, 102);
-$_DARK_GRAY_3 =  rgb(72, 72, 74);
-$_DARK_GRAY_4 =  rgb(58, 58, 60);
-$_DARK_GRAY_5 =  rgb(44, 44, 46);
-$_DARK_GRAY_6 =  rgb(28, 28, 40);
-
-$color_array = [  "blue"=> $_DEFAULT_BLUE,
-    "green"=> $_DEFAULT_GREEN,
-    "indigo"=> $_DEFAULT_INDIGO,
-    "orange"=> $_DEFAULT_ORANGE,
-    "pink"=> $_DEFAULT_PINK,
-    "purple"=> $_DEFAULT_PURPLE,
-    "red"=> $_DEFAULT_RED,
-    "teal"=> $_DEFAULT_TEAL,
-    "yellow"=> $_DEFAULT_YELLOW,
-    "gray"=> $_DEFAULT_GRAY_1,
-    "gray-1"=> $_DEFAULT_GRAY_2,
-    "gray-2"=> $_DEFAULT_GRAY_3,
-    "gray-3"=> $_DEFAULT_GRAY_4,
-    "gray-4"=> $_DEFAULT_GRAY_5,
-    "gray-5"=> $_DEFAULT_GRAY_6,
-    "gray-6"=> $_DARK_GRAY_2,
-    "gray-7"=> $_DARK_GRAY_3,
-    "gray-8"=> $_DARK_GRAY_4,
-    "gray-9"=> $_DARK_GRAY_5,
-    "gray-10"=> $_DARK_GRAY_6,];
+$color_array = [
+    "blue",
+    "green",
+    "indigo",
+    "orange",
+    "pink",
+    "purple",
+    "red",
+    "teal",
+    "yellow",
+    "gray",
+    "gray-1",
+    "gray-2",
+    "gray-3",
+    "gray-4",
+    "gray-5",
+    "gray-6",
+    "gray-7",
+    "gray-8",
+    "gray-9",
+    "gray-10",
+];
 
 $border_map = [
   "none",
@@ -70,6 +34,15 @@ $border_map = [
   "ridge",
   "inset",
   "outset",
+];
+
+$border_radius = [
+  "sm",
+  "md",
+  "lg",
+  "xl",
+  "none",
+  "full"
 ];
 
 ?>
@@ -92,7 +65,7 @@ $border_map = [
     <body>
         <div style="width: 1200px; margin: 0 auto;">
             <?php
-            foreach($color_array as $item => $value) {
+            foreach($color_array as $item) {
                 echo "<div class='b bc-$item'>border $item</div>" . '</br>';
             }
 
@@ -120,6 +93,10 @@ $border_map = [
 
             for($i = 1; $i <= 5; $i++) {
                 echo "<div class='b bw-$i'>border $i</div>" . '</br>';
+            }
+
+            foreach($border_radius as $radius) {
+                echo "<div class='b br-$radius'>border radius-$radius</div>" . '</br>';
             }
             ?>
         </div>
